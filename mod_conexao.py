dc913596ba822697f3c913aac7dc17ad05b15db0
mestredosucoalecrim-design <mestredosucoalecrim@gmail.com>
2026-09-11 21:01:47 -0300

    Update Supabase key retrieval method

--- mod_conexao.py
+++ mod_conexao.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 URL_SUPABASE = os.getenv("SUPABASE_URL")
-CHAVE_SUPABASE = os.get.env("SUPABASE_KEY")
+CHAVE_SUPABASE = st.secrets.get("SUPABASE_KEY")
 
 def criar_conexao() -> Client:
     """Estabelece e retorna a conexão oficial com o cliente Supabase."""
