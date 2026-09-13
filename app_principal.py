c6aab85d5c9a92eab866ee927b09934f51a09bb4
mestredosucoalecrim-design <mestredosucoalecrim@gmail.com>
2026-09-13 01:19:04 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app_principal.py
+++ app_principal.py
@@ -480,7 +480,7 @@ else:
             else:
                 st.warning("⚠️ Digite o nome do banco antes de gravar.")
 
-        elif resultado_banco == "duplicado":
+            elif resultado_banco == "duplicado":
                         st.warning(f"⚠️ Operação Recusada: A conta '{novo_banco_nome.upper()}' já existe no sistema.")
             else:
                 st.error("❌ O servidor rejeitou a gravação. Verifique a conexão.")
