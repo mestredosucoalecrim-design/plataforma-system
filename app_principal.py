acedd7f12750bf07c0ab44984b0d86d4fafccd8a
mestredosucoalecrim-design <mestredosucoalecrim@gmail.com>
2026-09-15 19:52:39 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- app_principal.py
+++ app_principal.py
@@ -136,8 +136,8 @@ if opcao_menu == "🏠 Menu Principal":
         # 🟢 AJUSTADO: Mudamos de 'if' para 'elif' para o sistema Hide funcionar e limpar a tela anterior!
         elif opcao_menu == "📈 Painel e Extratos":
         
-        # 🚀 VELOCIDADE E SEGURANÇA: Passa o ID único do usuário ativo para o filtro
-        with st.spinner("Sincronizando base histórica com a nuvem..."):
+            # 🚀 VELOCIDADE E SEGURANÇA: Passa o ID único do usuário ativo para o filtro
+            with st.spinner("Sincronizando base histórica com a nuvem..."):
             # CORREÇÃO: Enviamos o st.session_state.usuario_id para o motor!
             df_global = mod_calculos.buscar_todos_lancamentos_completos(st.session_state.usuario_id)
             resumo = mod_calculos.calcular_resumo_memoria(df_global)
