af1cbb73552a4ccac3efd630b708a30056f9f1ba
mestredosucoalecrim-design <mestredosucoalecrim@gmail.com>
2026-09-15 14:52:31 -0300

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- app_principal.py
+++ app_principal.py
@@ -321,7 +321,7 @@ else:
                         st.rerun()
 
     # --- TELA 2: NOVO LANÇAMENTO ---
-    elif opcao_menu == "Novo Lançamento":
+    elif opcao_menu == "📥 Novo Lançamento":
         # Criamos colunas invisíveis para "espremer" o formulário no centro, simulando um UserForm do VBA!
         col_margem_esq, col_formulario_central, col_margem_dir = st.columns([0.1, 0.8, 0.1])
         
