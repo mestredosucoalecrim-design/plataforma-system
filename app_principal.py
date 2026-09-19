8017be059a244114b75d74b7cd5a5f93c6a66a2b
William Melo <mestredosucoalecrim@gmail.com>
2026-09-19 00:06:41 -0300

    Alteração linha 34

--- app_principal.py
+++ app_principal.py
@@ -31,7 +31,7 @@ st.set_page_config(
 if "logado" not in st.session_state:
     st.session_state.logado = False
 if "ano_atual" not in st.session_state:
-    st.session_state.ano_atual = datetime.date.today().year
+    st.session_state.ano_atual = datetime.now().date().today().year
 if "mes_atual" not in st.session_state:
     st.session_state.mes_atual = datetime.date.today().month
 
